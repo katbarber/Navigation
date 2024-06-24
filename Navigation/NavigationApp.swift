c2ae982a891b3c0a3cb856993274a1750c9382d3
Kat <kbarber876@gmail.com>
2024-06-24 09:58:57 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Kat Barber on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
